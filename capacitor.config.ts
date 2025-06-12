2a2ee31bc6fa3f6125be65ef4a234197df27b80b
Tavakoli <tavakoli.mohsen74@gmail.com>
2025-06-13 02:58:20 +0330

    feat: First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.viracompany.jasmine',
+  appName: 'jasmine',
+  webDir: 'public'
+};
+
+export default config;
