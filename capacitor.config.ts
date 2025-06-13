9215af4cff6bab6fa054b7ac21fca3e779ce525c
Tavakoli <tavakoli.mohsen74@gmail.com>
2025-06-13 04:47:24 +0330

    feat: Check change on vercel

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'co.viracompany.jasmine',
-  appName: 'jasmine',
-  webDir: 'public'
+  appId: "co.viracompany.jasmine",
+  appName: "jasmine",
+  webDir: "public",
+  server: {
+    url: "https://next-pwa-capacitor.vercel.app/",
+    cleartext: false,
+  },
 };
 
 export default config;
