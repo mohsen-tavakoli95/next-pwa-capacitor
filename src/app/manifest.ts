49c1d56e588934cc8db640b9593650b4c6c3271d
Tavakoli <tavakoli.mohsen74@gmail.com>
2025-06-13 16:23:49 +0330

    feat: Change logo of the project

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,16 +9,77 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#000000",
+    // icons: [
+    //   {
+    //     src: "/assets/icons/logo/web-app-manifest-192x192.png",
+    //     sizes: "192x192",
+    //     type: "image/png",
+    //   },
+    //   {
+    //     src: "/assets/icons/logo/web-app-manifest-512x512.png",
+    //     sizes: "512x512",
+    //     type: "image/png",
+    //   },
+    // ],
     icons: [
       {
-        src: "/web-app-manifest-192x192.png",
-        sizes: "192x192",
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "48x48",
         type: "image/png",
       },
       {
-        src: "/web-app-manifest-512x512.png",
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "128x128",
+        type: "image/png",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-512x512.png",
+        sizes: "384x384",
+        type: "image/png",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/assets/icons/logo/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
