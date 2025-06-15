2988bafa852d57c862b7844a12296bb7d8c6b0cc
Tavakoli <tavakoli.mohsen74@gmail.com>
2025-06-15 03:31:28 +0330

    feat: Test Input in pwa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 // import Image from "next/image";
-
+import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-[100dvh]">
-      <p className="font-bold text-xl">welcone to Jasmine</p>
+      <p className="font-bold text-xl  bg-red-500 standalone:bg-black">
+        welcone to Jasmines
+      </p>
+      <Input placeholder="text" />
     </div>
   );
 }
