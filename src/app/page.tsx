b3dd61964fea8ec481441ce4318e5972ba5874f2
Tavakoli <tavakoli.mohsen74@gmail.com>
2025-06-17 21:26:57 +0330

    feat: Add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 // import Image from "next/image";
 import { Input } from "@/components/ui/input";
+//navigation
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  redirect("/auth/login");
   return (
     <div className="flex items-center justify-center h-[100dvh]">
       <p className="font-bold text-xl  bg-red-500 standalone:bg-black">
